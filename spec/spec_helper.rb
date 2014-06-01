616b12bfb70dcde91a81ee307fd8c65a436c8f7d
LNA <LAllen@8thLight.com>
2014-06-01 18:22:42 -0500

    Adds spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,16 @@
-require 'rspec'
- 
-Dir["./lib/*.rb"].each { |file| require file }
+$: << File.expand_path(File.dirname(__FILE__)) + '/../'
+$: << File.expand_path(File.dirname(__FILE__)) + '/../lib/'
+
+require 'app'
+require 'rack/test'
+require 'web_game_store'
+
+require 'pry'
+
+def app
+  App
+end
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+end

4bca65b437d886ccd5bb24ce1cd3d17209ed2ec4
LNA <LAllen@8thLight.com>
2014-06-01 18:41:10 -0500

    Requires sinatra bootstrap

--- app.rb
+++ app.rb
@@ -2,9 +2,16 @@ $: << File.expand_path(File.dirname(__FILE__)) + '/lib'
 Dir[File.dirname(__FILE__) + '/lib/models/*.rb'].each {|file| require file }
 
 require 'sinatra/base'
+require 'sinatra/twitter-bootstrap'
 require 'settings'
 
-class App < Sinatra::Application
+class App < Sinatra::Base
+  register Sinatra::Twitter::Bootstrap::Assets
+
+  use Rack::CommonLogger
+
+  enable :inline_templates
+
   get '/' do
     erb '/welcome'.to_sym
   end

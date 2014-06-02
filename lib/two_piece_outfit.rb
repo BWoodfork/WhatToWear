316493f84c19ed2253a9fdc2e853f89a2e106cca
LNA <LAllen@8thLight.com>
2014-06-01 20:55:55 -0500

    Fixes initializer

--- lib/two_piece_outfit.rb
+++ lib/two_piece_outfit.rb
@@ -1,7 +1,7 @@
 class TwoPieceOutfit
   def initialize(piece_one, piece_two, accessories)
-    @piece_one = piece_one
-    @piece_two = piece_two
-    @accessories
+    @piece_one   = piece_one
+    @piece_two   = piece_two
+    @accessories = accessories
   end
 end

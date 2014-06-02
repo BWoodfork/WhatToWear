af707d6508d4b94f13fc06cb2113c0daf4ee12ea
LNA <LAllen@8thLight.com>
2014-06-01 22:00:03 -0500

    Refactors bruch with friends settings

--- lib/outfit_options.rb
+++ lib/outfit_options.rb
@@ -1,7 +1,6 @@
 class OutfitOptions
   def find_outfit_for(settings)
-    if settings == { :occasion=>"brunch", 
-                     :with_who=>"friends"}
+    if settings == brunch_with_friends_settings
       outfit = brunch_with_friends
     end
     outfit
@@ -9,5 +8,10 @@ class OutfitOptions
 
   def brunch_with_friends
     "Long grey dress, teal bag, flip flops."
+  end
+
+  def brunch_with_friends_settings
+    { :occasion=>"brunch", 
+      :with_who=>"friends"}
   end
 end

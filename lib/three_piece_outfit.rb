34a684e31e79a02d7a6eec9676945e90b84e7757
LNA <LAllen@8thLight.com>
2014-06-01 20:40:19 -0500

    Adds class for 3 piece outfit

---
+++ lib/three_piece_outfit.rb
@@ -0,0 +1,8 @@
+class ThreePieceOutfit
+  def initialize(piece_one, piece_two, piece_three, accessories)
+    @piece_one   = piece_one
+    @piece_two   = piece_two
+    @piece_three = piece_three
+    @piece_four  = piece_four
+  end
+end

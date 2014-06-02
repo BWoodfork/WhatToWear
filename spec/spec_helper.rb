19eb46d3d11c817838d8fce7e92da7441fb48ab6
LNA <LAllen@8thLight.com>
2014-06-01 21:58:11 -0500

    Removes unneeded requires

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,8 @@ $: << File.expand_path(File.dirname(__FILE__)) + '/../'
 $: << File.expand_path(File.dirname(__FILE__)) + '/../lib/'
 
 require 'app'
-require 'one_piece_outfit'
 require 'outfit_options'
 require 'rack/test'
-require 'three_piece_outfit'
-require 'two_piece_outfit'
 
 require 'pry'
 
